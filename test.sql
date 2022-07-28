7d943b0aa21e3192af794c7437f849f6c19e0e63
Fegi Sucepto Priawan <fegisucepto6997@gmail.com>
2022-07-29 02:41:38 +0700

    test sql proses

---
+++ test.sql
@@ -0,0 +1,15 @@
+
+select id, username, email, createdAt from users u  where createdAt = now()
+
+select * from users u
+
+select id, username, email, createdAt from users u
+
+       
+select UserId , balance  from wallets w 
+
+SELECT user,host FROM mysql.user;
+
+
+
+
